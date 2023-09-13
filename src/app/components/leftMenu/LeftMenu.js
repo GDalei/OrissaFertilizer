7ac8109863d4d23a750df763bf285f8d78ed37c9
Ganesh Dalei <ganesh.dalei@kriritindia.com>
2023-09-13 10:31:59 -0700

    fixed

--- src/app/components/leftMenu/LeftMenu.js
+++ src/app/components/leftMenu/LeftMenu.js
@@ -3,13 +3,13 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
-import style from './style.module.css';
-// const style = {
-//     width: '100%',
-//     maxWidth: 360,
-//     bgcolor: 'background.paper',
-//     fontSize: "12px",
-// };
+
+const style = {
+    width: '100%',
+    maxWidth: 360,
+    bgcolor: 'background.paper',
+    fontSize: "12px",
+};
 
 export default function LeftMenu() {
     return (
